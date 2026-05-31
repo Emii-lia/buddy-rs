8e3856e9f4d88791e401ec2b23447daeca8b7686
Emii-lia <fiaromiangaly@gmail.com>
2026-05-31 15:59:10 +0300

    feat(reaction-rules): implementin git command rule

---
+++ daemon/src/reaction/rules/git.rs
@@ -0,0 +1,15 @@
+use shared::types::Event;
+use crate::reaction::traits::ReactionRule;
+
+pub struct GitRule;
+
+impl ReactionRule for GitRule {
+  fn matches(&self, event: &Event) -> bool {
+    matches!(event, Event::Command(cmd) if cmd.command.contains("git "))
+  }
+  fn react(&self, _event: &Event) -> Option<String> {
+    let buddy = "(⁠◠⁠‿⁠・⁠)";
+    let msg = "Recording today's mistake for future reference.";
+    Some(format!("{} {}", msg, buddy))
+  }
+}
