1d62d3e0d3c354e28c502d171b0f5a7c29379b9b
Emii-lia <fiaromiangaly@gmail.com>
2026-06-01 10:30:58 +0300

    refactor(reaction-rules): updating git rule observer reaction message

--- daemon/src/reaction/rules/git.rs
+++ daemon/src/reaction/rules/git.rs
@@ -1,15 +1,16 @@
 use shared::types::Event;
+use crate::reaction::style::wrap_in_bubble;
 use crate::reaction::traits::ReactionRule;
 
 pub struct GitRule;
 
 impl ReactionRule for GitRule {
   fn matches(&self, event: &Event) -> bool {
-    matches!(event, Event::Command(cmd) if cmd.command.contains("git "))
+    matches!(event, Event::Command(cmd) if cmd.command.contains("git"))
   }
   fn react(&self, _event: &Event) -> Option<String> {
-    let buddy = "(⁠◠⁠‿⁠・⁠)";
+    let buddy = "(◠‿ o )";
     let msg = "Recording today's mistake for future reference.";
-    Some(format!("{} {}", msg, buddy))
+    Some(wrap_in_bubble(msg, &buddy))
   }
 }
