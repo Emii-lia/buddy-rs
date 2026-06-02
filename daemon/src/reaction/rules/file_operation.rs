d44ceecd2e987a135cabad6fbb581a71d0a7e9a8
Emii-lia <fiaromiangaly@gmail.com>
2026-06-02 13:02:08 +0300

    refactor(reaction): adjusting file operation rule logics

--- daemon/src/reaction/rules/file_operation.rs
+++ daemon/src/reaction/rules/file_operation.rs
@@ -1,5 +1,5 @@
 use shared::types::Event;
-use crate::reaction::style::wrap_in_bubble;
+use shared::style::wrap_in_bubble;
 use crate::reaction::traits::ReactionRule;
 
 pub struct FileOperationRule;
@@ -15,11 +15,11 @@ impl ReactionRule for FileOperationRule {
 
   fn react(&self, event: &Event) -> Option<String> {
     let Event::Command(cmd) = event;
-    if ["rm ", "rmdir "].iter().any(|op| cmd.command.contains(op)) {
+    if ["rm", "rmdir"].contains(&cmd.command.split_whitespace().next().unwrap()) {
       let msg = "Deleting evidence";
       let buddy = "(・_・;)";
       Some(wrap_in_bubble(msg, buddy))
-    } else if ["mv ", "cp ", "ln "].iter().any(|op| cmd.command.contains(op)) {
+    } else if ["mv", "cp", "ln"].contains(&cmd.command.split_whitespace().next().unwrap()) {
       let msg = "Everything is exactly where you left it. Probably.";
       let buddy = "╮(╯_╰)╭";
       Some(wrap_in_bubble(msg, buddy))
